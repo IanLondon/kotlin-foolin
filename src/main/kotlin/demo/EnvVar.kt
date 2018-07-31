33c7c788b7cde4b2a8c4f3b2fbbbc57c4cbfa0f9
IanLondon <ianlondondu@gmail.com>
2018-07-31 00:13:13 -0400

    initial commit w/ env var + args + file example

---
+++ src/main/kotlin/demo/EnvVar.kt
@@ -0,0 +1,26 @@
+package demo
+
+import java.io.File
+import java.io.BufferedReader
+
+fun main (args: Array<String>) {
+    val envVar = System.getenv("TEST_VAR")
+    println("""
+        |Heyyy your args are "${args.joinToString(separator = ", ")}"
+        |And your TEST_VAR is "$envVar"
+        |
+    """.trimMargin())
+
+    val fileToRead = System.getenv("TEST_FILE")
+    if (fileToRead != null) {
+        val relativePath = File(System.getProperty("user.dir"))
+        val file = relativePath.resolve(fileToRead)
+        println("Reading file from path $file\n\n*****\n")
+
+        file.useLines { println(it.joinToString("\n")) }
+
+    } else {
+        println("TEST_FILE not specified!")
+    }
+
+}
